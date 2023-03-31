7c4736a6bea22b3843a1829e8aaa69eb335f4bb4
Lucas Ruotolo Ardevol <120602781+lucasArdevol@users.noreply.github.com>
2023-03-31 19:57:53 -0300

    Add files via upload

---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci(num):
+    a, b = 0, 1
+    while a <= num:
+        if a == num:
+            return True
+        a, b = b, a+b
+    return False
+
+num = int(input("Informe um número: "))
+if fibonacci(num):
+    print(f"O número {num} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {num} não pertence à sequência de Fibonacci.")
